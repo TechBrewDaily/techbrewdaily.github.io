852ab54a15538d1d509f692a57431c5f9a989566
jainhardik06 <2023pietcshardik060@poornima.org>
2025-06-25 00:10:24 +0530

    Clean config + GH Actions-only deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,9 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-// The 'rss' import has been removed.
 
 export default defineConfig({
   site: 'https://jainhardik06.github.io',
   base: '/astro-blog-techbrewdaily/',
-
-  integrations: [
-    mdx(),
-    sitemap(),
-    // The entire rss({...}) block has been removed.
-  ],
+  integrations: [mdx(), sitemap()],
 });
